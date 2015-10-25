2e79d9ce721532f02ca0d20fe6ea667e21a4ddf3
Elviss Strazdins <elviss@elviss.lv>
2015-10-25 20:57:08 +0200

    Add Eclipse settings Add APDU constants

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
-import java.util.List;
+import javax.smartcardio.Card;
+import javax.smartcardio.CardChannel;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+import javax.smartcardio.TerminalFactory;
 
-import javax.smartcardio.*;
+import java.util.List;
 
 public class Application
 {
@@ -31,6 +36,39 @@ public class Application
 		_terminal = terminals.get(terminalId);
 	}
 	
+	public void dumpBytes(byte val[])
+	{
+		for (int i = 0; i < val.length; ++i)
+		{
+			if (i != 0)
+			{
+				System.out.print(", ");
+			}
+
+			//System.out.print(val[i]);
+			System.out.print(String.format("%02X ", val[i]));
+		}
+		System.out.print("\n");
+	}
+	
+	public String parseUID(byte val[])
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < val.length; i++)
+		{
+            byte b = val[i];
+            
+            if (i <= val.length - 3)
+            {
+            	// append uid
+            	sb.append(String.format("%02X", b));
+            }
+		}
+		
+		return sb.toString();
+	}
+	
 	public void run() throws Exception
 	{
 		while (true)
@@ -41,11 +79,16 @@ public class Application
 				
 				try
 				{
-					card = _terminal.connect("*");
+					card = _terminal.connect("*"); // T={protocol}
 				}
 				catch (Exception e)
 				{
 					e.printStackTrace();
+					
+					if (_terminal.waitForCardAbsent(0))
+					{
+						System.out.println("Card disconnected");
+					}
 				}
 				
 				if (card != null)
@@ -54,22 +97,52 @@ public class Application
 					
 					try
 					{
-						byte[] getDataCommand = {(byte)0xff, (byte)0xca, (byte)0x00, (byte)0x00, (byte)0x00};
-						
 						CardChannel channel = card.getBasicChannel();
-						ResponseAPDU r = channel.transmit(new CommandAPDU(getDataCommand));
-						System.out.println("response: " + r.getBytes());
+						ResponseAPDU r;
 						
-						byte response[] = r.getBytes();
+						byte[] uidCommand = { (byte)Apdu.CLS_PTS, (byte)Apdu.INS_GET_DATA, (byte)0x00, (byte)0x00, (byte)0x00 };
 						
-						for (int i = 0; i < response.length; ++i)
-						{
-							System.out.println(response[i]);
-						}
+						r = channel.transmit(new CommandAPDU(uidCommand));
+						System.out.println("UID response: " + r);
+						System.out.println("UID: " + parseUID(r.getBytes()));
 						
-						byte[] command = {(byte)0x00, (byte)0xca, (byte)0x00, (byte)0x40, (byte)0xff};
-						ResponseAPDU answer = channel.transmit(new CommandAPDU(command));
-						System.out.println("answer: " + answer.toString());
+						
+						byte uidBytes[] = card.getATR().getBytes();
+						
+						dumpBytes(uidBytes);
+						System.out.println("UID: " + parseUID(uidBytes));
+						
+						byte[] historyCommand = { (byte)Apdu.CLS_PTS, (byte)Apdu.INS_GET_DATA, (byte)0x01, (byte)0x00, (byte)0x00 };
+						
+						r = channel.transmit(new CommandAPDU(historyCommand));
+						System.out.println("Hystorical bytes response: " + r.toString());
+
+						dumpBytes(r.getBytes());
+						
+						byte histricalBytes[] = card.getATR().getHistoricalBytes();
+						
+						dumpBytes(histricalBytes);
+						
+						
+						byte IDENTITY_FILE_AID[] = {   
+							   (byte) 0x3F,// MASTER FILE, Head directory MF "3f00"  
+							   (byte) 0x00,   
+							   (byte) 0xDF,// Dedicated File, subdirectory identity DF(ID) "DF01"  
+							   (byte) 0x01,   
+							   (byte) 0x40,// Elementary File, the identity file itself EF(ID#RN) "4031"  
+							   (byte) 0x31 };
+						
+						CommandAPDU selectFileApdu = new CommandAPDU(   
+								0x00, //CLA   
+								0xA4, //INS  
+								0x08, //P1  
+								0x0C, //P2  
+								IDENTITY_FILE_AID);  
+						
+						r = channel.transmit(selectFileApdu);
+						System.out.println("response: " + r.toString());
+
+						dumpBytes(r.getBytes());
 					}
 					catch (Exception e)
 					{
