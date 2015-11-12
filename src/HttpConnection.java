13b90b36654312d2997e9c565bd0d490db725f12
Elviss Strazdins <elviss@elviss.lv>
2015-11-12 20:55:18 +0200

    Fix import warning

--- src/HttpConnection.java
+++ src/HttpConnection.java
@@ -1,5 +1,5 @@
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
+//import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
